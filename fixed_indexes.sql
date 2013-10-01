245ae2be3e7ed2f9e03a74d411df06fbcd39e211
Sayan Malakshinov <xt.and.r@gmail.com>
2013-10-01 17:23:57 +0400

    formatting

--- fixed_indexes.sql
+++ fixed_indexes.sql
@@ -1,4 +1,15 @@
-select * 
-from v$indexed_fixed_column
-where table_name='&1'
+col TABLE_NAME  format a30;
+col COLUMN_NAME format a30;
+break on TABLE_NAME on INDEX_NUMBER skip 1
+select 
+    fc.TABLE_NAME
+   ,fc.INDEX_NUMBER
+   ,fc.COLUMN_POSITION
+   ,fc.COLUMN_NAME
+from v$indexed_fixed_column fc
+where table_name like upper('&1')
+order by 1,2,3
 /
+clear break;
+col TABLE_NAME  clear;
+col COLUMN_NAME clear;

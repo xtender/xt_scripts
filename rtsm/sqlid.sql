5b3c216654309283ca8703b049bda0d219e03408
Sayan Malakshinov <xt.and.r@gmail.com>
2013-10-23 16:03:57 +0400

    workaround for bug with: ERROR: ORA-06502: PL/SQL: numeric or value error ORA-06512: at "SYS.DBMS_ASSERT", line 342 ORA-06512: at "SYS.DBMS_ASSERT", line 411 ORA-06512: at "SYS.DBMS_REPORT", line 1185 ORA-06512: at "SYS.DBMS_REPORT", line 1253 ORA-06512: at "SYS.DBMS_SQLTUNE", line 13966 ORA-06502: PL/SQL: numeric or value error ORA-06512: at "SYS.DBMS_SQLTUNE", line 14265 ORA-06512: at line 1

--- rtsm/sqlid.sql
+++ rtsm/sqlid.sql
@@ -1,4 +1,26 @@
+@inc/input_vars_init;
 col sqlmon format a240;
-select dbms_sqltune.report_sql_monitor(sql_id => '&1',report_level => 'ALL',type => 'TEXT') sqlmon from dual;
+
+-- workaround for bug with:
+-- ORA-06502: PL/SQL: numeric or value error
+-- ORA-06512: at "SYS.DBMS_SQLTUNE", line 14265
+set termout off;
+col nls_num_chars new_val nls_num_chars noprint;
+select value nls_num_chars from nls_session_parameters where parameter='NLS_NUMERIC_CHARACTERS';
+alter session set nls_numeric_characters='.,';
+
+
+-- main part:
+set termout on;
+select/*+ no_monitor */ dbms_sqltune.report_sql_monitor(sql_id => '&1',report_level => 'ALL',type => 'TEXT') sqlmon from dual;
+set termout off
+-- end main part
+
+
+-- restore all:
+alter session set nls_numeric_characters='&nls_num_chars';
 col sqlmon clear;
-undef 1;
+undef 1 nls_num_chars;
+
+set termout on;
+@inc/input_vars_undef;

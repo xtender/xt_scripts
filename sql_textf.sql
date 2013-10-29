b1a54212cd28b4cac6ed18b72bdc8ddeb36defcd
Sayan Malakshinov <xt.and.r@gmail.com>
2013-10-30 01:14:16 +0400

    New sql beautifier

--- sql_textf.sql
+++ sql_textf.sql
@@ -1,7 +1,7 @@
 set timing off head off
 col qtext format a150
 prompt ################################  Original query text:  ################################################;
-spool tmp/to_format.sql
+spool &_SPOOLS/to_format.sql
 select
     coalesce(
         (select sql_fulltext from v$sqlarea a where a.sql_id='&1')
@@ -12,6 +12,6 @@ from dual
 spool off
 
 prompt ################################  Formatted query text #################################################;
-host perl inc/sql_format_standalone.pl tmp/to_format.sql
+host perl inc/sql_format_standalone.pl &_SPOOLS/to_format.sql
 prompt ################################  Formatted query text End #############################################;
 set termout on head on

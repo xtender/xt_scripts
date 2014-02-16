112674192959150fe25e79d9a581bab82b1b9b30
Sayan Malakshinov <xt.and.r@gmail.com>
2014-02-16 19:45:50 +0400

    +status

--- obj.sql
+++ obj.sql
@@ -3,7 +3,7 @@ col owner       format a30;
 col object_name format a30;
 col object_type format a25;
 
-select owner,object_name,object_type,object_id ,created,last_ddl_time,o.timestamp
+select owner,object_name,object_type,object_id ,created,last_ddl_time,o.timestamp,status
 from dba_objects o 
 where 
    (to_char(o.object_id) like '&1' or upper(object_name) like upper('&1'))

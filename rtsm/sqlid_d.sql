e4aefd8216938fdd7b405bfd01181a91cb5604a4
Sayan Malakshinov <xt.and.r@gmail.com>
2014-04-09 17:04:05 +0400

    dups removed

--- rtsm/sqlid_d.sql
+++ rtsm/sqlid_d.sql
@@ -3,13 +3,6 @@ prompt eXplain with Profile: Running DBMS_SQLTUNE.REPORT_SQL_DETAIL for SQL_ID='
 @inc/input_vars_init;
 set termout off timing off ver off feed off head off lines 10000000 pagesize 0
 -------------------------
--- workaround for bug with:
--- ORA-06502: PL/SQL: numeric or value error
--- ORA-06512: at "SYS.DBMS_SQLTUNE", line 14265
-col nls_num_chars new_val nls_num_chars noprint;
-select value nls_num_chars from nls_session_parameters where parameter='NLS_NUMERIC_CHARACTERS';
-alter session set nls_numeric_characters='.,';
--------------------------
 define MONSQLID=&1
 spool &_TEMPDIR\xprof_d_&MONSQLID..html
 
@@ -17,9 +10,6 @@ spool &_TEMPDIR\xprof_d_&MONSQLID..html
 
 spool off
 host &_START &_TEMPDIR\xprof_d_&MONSQLID..html
--------------------------
-alter session set nls_numeric_characters='&nls_num_chars';
-undef nls_num_chars;
 -------------------------
 undef MONSQLID
 @inc/input_vars_undef;

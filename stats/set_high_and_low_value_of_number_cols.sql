34025a2d40d56a2db15701f9849d8991f978c935
Sayan Malakshinov <xt.and.r@gmail.com>
2014-10-11 01:29:56 +0400

    for nums

---
+++ stats/set_high_and_low_value_of_number_cols.sql
@@ -0,0 +1,122 @@
+accept tab_owner - 
+		prompt 'Enter value for tab_owner[&_USER]: ' -
+		default '&_USER';
+
+accept tab_name - 
+		prompt 'Enter value for tab_name: ';
+
+accept col_name - 
+		prompt 'Enter value for col_name: ';
+
+accept v_low - 
+		prompt 'Enter value for low value: ';
+
+accept v_high - 
+		prompt 'Enter value for high value: ';
+
+set serverout on;
+
+DECLARE
+   n_distcnt          NUMBER;
+   n_density          NUMBER;
+   n_nullcnt          NUMBER;
+   rec_srec           DBMS_STATS.statrec;
+   vals               DBMS_STATS.numarray;
+   n_avgclen          NUMBER;
+   d_low              NUMBER;
+   d_high             NUMBER;
+   r_low              RAW(4000);
+   r_high             RAW(4000);
+   /* functions for cast raw */
+   function raw_to_num(i_raw raw)
+   return varchar2
+   as
+      m_n number;
+   begin
+      dbms_stats.convert_raw_value(i_raw,m_n);
+      return m_n;
+   exception when others then return 'ERROR:'||sqlerrm;
+   end;
+     
+   function raw_to_date(i_raw raw)
+   return date
+   as
+      m_n date;
+   begin
+      dbms_stats.convert_raw_value(i_raw,m_n);
+      return m_n;
+   end;
+     
+   function raw_to_varchar2(i_raw raw)
+   return varchar2
+   as
+      m_n varchar2(4000);
+   begin
+      dbms_stats.convert_raw_value(i_raw,m_n);
+      return m_n;
+   end;
+   /* end functions */
+BEGIN
+   DBMS_STATS.get_column_stats ('&tab_owner',
+                                '&tab_name',
+                                '&col_name',
+                                distcnt      => n_distcnt,
+                                density      => n_density,
+                                nullcnt      => n_nullcnt,
+                                srec         => rec_srec,
+                                avgclen      => n_avgclen
+                               );
+    
+   --handles just the case when HISTOGRAMS are not set -> rec_srec.epc = 2
+   IF rec_srec.epc = 2 THEN
+     SELECT low_value, high_value
+        INTO r_low,r_high
+        FROM dba_tab_col_statistics s
+      WHERE 
+            s.owner       = '&tab_owner'
+        and s.table_name  = '&tab_name' 
+        AND s.column_name = '&col_name';
+     
+     d_low := raw_to_num(r_low);
+     d_high:= raw_to_num(r_high);
+     
+     dbms_output.put_line('Old low  value: '|| to_char(d_low ));
+     dbms_output.put_line('Old high value: '|| to_char(d_high));
+     
+     d_low  := &v_low ;
+     d_high := &v_high;
+          
+     vals := DBMS_STATS.numarray(d_low, d_high);
+     rec_srec.minval:=NULL;
+     rec_srec.maxval:=NULL;
+     rec_srec.bkvals:=NULL;
+     rec_srec.novals:=NULL;
+      
+     --this procedure will set epc.minval, epc.maxval etc in INTERNAL FORMAT
+     DBMS_STATS.PREPARE_COLUMN_VALUES(rec_srec, vals);
+      
+     --and then just set statistics
+     DBMS_STATS.set_column_stats ('&tab_owner',
+                                  '&tab_name',
+                                  '&col_name',
+                                  distcnt      => n_distcnt,
+                                  density      => n_density,
+                                  nullcnt      => n_nullcnt,
+                                  srec         => rec_srec,
+                                  avgclen      => n_avgclen,
+                                  force        => true
+                                  );
+     dbms_output.put_line('New low  value: '|| to_char(d_low ));
+     dbms_output.put_line('New high value: '|| to_char(d_high));
+   ELSE
+     raise_application_error(-20001,'!!!!! There are more than 1 bucket !!!!!!');
+   END IF;   
+END;
+/
+undef tab_owner;
+undef tab_name;
+undef col_name;
+undef v_low;
+undef v_high;
+
+set serverout off;

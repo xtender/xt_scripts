19bd7674d31752bc67d7e39cbc9ba33354d70b5b
Sayan Malakshinov <xt.and.r@gmail.com>
2014-10-29 17:42:49 +0300

    package body

--- sql_plsql_obj.sql
+++ sql_plsql_obj.sql
@@ -1,24 +1,26 @@
-col owner           for a10
-col object_name     for a30
-col text            for a120
+col owner           for a10;
+col object_name     for a30;
+col object_type     for a20;
+col text            for a120;
 
 select 
    a.inst_id
   ,a.SQL_ID
-  ,p.owner,p.object_name
+  ,o.owner,o.object_name,o.object_type
   ,s.line
   ,rtrim(s.text,chr(10)) text
 from
     gv$sqlarea a
-    left join dba_procedures p
-              on a.PROGRAM_ID=p.OBJECT_ID
+    left join dba_objects o
+              on a.PROGRAM_ID = o.OBJECT_ID
     left join dba_source s
-              on p.owner=s.owner
-              and p.OBJECT_NAME=s.name
+              on o.owner=s.owner
+              and o.OBJECT_NAME=s.name
               and s.line between a.PROGRAM_LINE#-5 and a.PROGRAM_LINE#+5
 where a.SQL_ID='&1'
 order by 1,2,3,4,5
 /
-col owner           clear
-col object_name     clear
-col text            clear
+col owner           clear;
+col object_name     clear;
+col object_type     clear;
+col text            clear;

3d18fe6e9b2bda95fd00485b7c7372ce8b71a751
Sayan Malakshinov <xt.and.r@gmail.com>
2014-11-06 17:42:46 +0300

    +client_identifier

--- sessions.sql
+++ sessions.sql
@@ -6,6 +6,7 @@ col process       format a12;
 col program       format a20;
 col module        format a20;
 col terminal      format a20;
+col client_id     format a20;
 col type          format a10;
 col sql_id        format a13;
 col action        format a17;
@@ -21,7 +22,8 @@ select
     ,s.osuser,s.process
     ,substr(s.program,1,20) program
     ,s.module
-    ,s.terminal,s.type
+    ,s.terminal
+    ,s.client_identifier as client_id
 &_IF_ORA11_OR_HIGHER    ,to_char(s.SQL_EXEC_START,'dd/mm hh24:mi:ss') sql_exec_start
     ,s.sql_id
     ,s.action
@@ -29,6 +31,7 @@ select
     ,s.wait_class
     --,s.row_wait_obj#
     ,(select object_name from dba_objects o where object_id=s.row_wait_obj#) objname
+    ,s.type
 from gv$session s 
 where 
     (
@@ -40,12 +43,13 @@ where
   or 
     ('&1' is not null 
       and (
-              s.username      like upper('%'||'&1'||'%') 
-           or upper(osuser)   like upper('%'||'&1'||'%')
-           or upper(module)   like upper('%'||'&1'||'%')
-           or upper(terminal) like upper('%'||'&1'||'%')
-           or upper(action) like upper('%'||'&1'||'%')
-           or sql_id          like      ('%'||'&1'||'%')
+              s.username                like upper('%'||'&1'||'%') 
+           or upper(osuser)             like upper('%'||'&1'||'%')
+           or upper(module)             like upper('%'||'&1'||'%')
+           or upper(terminal)           like upper('%'||'&1'||'%')
+           or upper(action)             like upper('%'||'&1'||'%')
+           or upper(client_identifier)  like upper('%'||'&1'||'%')
+           or sql_id                    like      ('%'||'&1'||'%')
            )
       and ('&2' is null or (s.status='ACTIVE'  and s.wait_class!='Idle'))
     )
@@ -58,6 +62,7 @@ col process       clear;
 col module        clear;
 col program       clear;
 col terminal      clear;
+col client_id     clear;
 col type          clear;
 --col sql_id        clear;
 col action        clear;

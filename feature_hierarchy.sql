d5772b658667c4ebaa136ea788dda0499500f234
Sayan Malakshinov <xt.and.r@gmail.com>
2015-03-25 12:07:54 +0300

    v$sql_feature_hierarchy

---
+++ feature_hierarchy.sql
@@ -0,0 +1,24 @@
+col feature for a35;
+col path    for a100;
+WITH feature_hierarchy AS (
+   select
+      rownum n
+     ,f.sql_feature
+     ,substr(SYS_CONNECT_BY_PATH(REPLACE(f.sql_feature, 'QKSFM_'), ' -> '),5) path
+   FROM 
+       v$sql_feature f
+     , v$sql_feature_hierarchy fh 
+   WHERE 
+       f.sql_feature = fh.sql_feature 
+   CONNECT BY fh.parent_id = PRIOR f.sql_Feature 
+   START WITH fh.sql_feature = 'QKSFM_ALL'
+)
+select 
+   fh.path
+  ,REPLACE(fh.sql_feature, 'QKSFM_') as feature
+from feature_hierarchy fh
+where '&1' is null or regexp_like(path,'&1')
+order by n
+/
+col feature clear;
+col path    clear;

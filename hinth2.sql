ad39aa9e80c34db91392552d8722383a3ecf257a
Sayan Malakshinov <xt.and.r@gmail.com>
2015-04-09 17:40:46 +0300

    simplifying

--- hinth2.sql
+++ hinth2.sql
@@ -9,28 +9,28 @@ col version     for a8;
 col ver_outline for a8;
 
 WITH feature_hierarchy AS (
-   select--+ no_merge
+   select 
       rownum n
-     ,f.sql_feature
-     ,substr(SYS_CONNECT_BY_PATH(REPLACE(f.sql_feature, 'QKSFM_'), ' -> '),5) path
-   FROM 
-       v$sql_feature f
-     , v$sql_feature_hierarchy fh 
-   WHERE 
-       f.sql_feature = fh.sql_feature 
-   CONNECT BY fh.parent_id = PRIOR f.sql_Feature 
-   START WITH fh.sql_feature = 'QKSFM_ALL'
+     ,h.*
+     ,substr(SYS_CONNECT_BY_PATH(REPLACE(h.sql_feature, 'QKSFM_'), ' -> '),5) path_concatenated
+     ,rpad('  ',level*2,'..')||sql_feature                                    path_prefixed
+   from v$sql_feature_hierarchy h
+   start with
+         sql_feature='QKSFM_ALL'
+         --sql_feature='QKSFM_TRANSFORMATION'
+         --parent_id is null
+   connect by parent_id = prior sql_feature
 )
-select fh.path
-      ,hi.class           as hint_class
-      ,hi.name            as hint_name
-      ,hi.version         as version
-      ,hi.version_outline as ver_outline
+select fh.path_concatenated as path
+      ,hi.class             as hint_class
+      ,hi.name              as hint_name
+      ,hi.version           as version
+      ,hi.version_outline   as ver_outline
 from feature_hierarchy fh
     ,v$sql_hint hi
 where hi.sql_feature(+) = fh.sql_feature
-  and ('&_path_mask' is null or regexp_like(fh.path,'&_path_mask','i'))
-  and ('&_hint_mask' is null or regexp_like(hi.name,'&_hint_mask','i'))
+  and ('&_path_mask' is null or regexp_like(fh.path_concatenated,'&_path_mask','i'))
+  and ('&_hint_mask' is null or regexp_like(hi.name             ,'&_hint_mask','i'))
   and ('&_min_ver'   is null or version is null or regexp_replace(regexp_replace(version,'(\d+)','0000\1'),'\d*(\d{2})','\1')>= regexp_replace(regexp_replace('&_min_ver','(\d+)','0000\1'),'\d*(\d{2})','\1'))
 order by n, hint_class, hint_name, version
 /

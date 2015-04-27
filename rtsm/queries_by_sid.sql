862a4474ef059c9d4f37f6d5162b408706185984
Sayan Malakshinov <xt.and.r@gmail.com>
2015-04-27 17:52:28 +0300

    w.ver

--- rtsm/queries_by_sid.sql
+++ rtsm/queries_by_sid.sql
@@ -1,9 +1,29 @@
+col sql_id      for a13;
+col status      for a25 trunc;
+col sql_text    for a120 trunc;
 select 
-   r.sql_id
+   sql_id
   ,sql_exec_start
   ,sql_exec_id 
   ,status
-  ,elapsed_time
-  ,cpu_time
-from v$sql_monitor r 
-where r.sid=&1;
+  ,round(elapsed_time/1e6,3) elapsed_time
+  ,round(cpu_time/1e6,3)    cpu_time
+  ,sql_text
+from (
+      select 
+         sql_id
+        ,sql_exec_start
+        ,sql_exec_id 
+        ,status
+        ,elapsed_time
+        ,cpu_time
+        ,sql_text
+        ,dense_rank()over(order by sql_exec_start) rnk
+      from v$sql_monitor r 
+      where r.sid=&1
+) v
+where rnk<=20 or status='EXECUTING'
+;
+col sql_id      clear;
+col status      clear;
+col sql_text    clear;
---
+++ userinfo.sql
@@ -0,0 +1,39 @@
+col username            for a30;
+col password            noprint;
+col account_status      for a30;
+col external_name       for a30;
+col password_version    noprint;
+col default_ts          for a30;
+col temp_ts             for a30;
+col profile             for a30;
+col rsrc_group          for a30;
+select
+     USER_ID
+    ,USERNAME
+    ,PASSWORD
+    ,ACCOUNT_STATUS
+    ,CREATED
+    ,LOCK_DATE
+    ,EXPIRY_DATE
+    ,DEFAULT_TABLESPACE     as default_ts
+    ,TEMPORARY_TABLESPACE   as temp_ts
+    ,PROFILE
+    ,INITIAL_RSRC_CONSUMER_GROUP as rsrc_group
+    ,EXTERNAL_NAME
+    ,PASSWORD_VERSIONS
+    ,EDITIONS_ENABLED
+    ,AUTHENTICATION_TYPE
+from dba_users u
+where 
+upper(u.username) like upper('&1')
+order by user_id
+/
+col username            clear;
+col password            clear;
+col account_status      clear;
+col external_name       clear;
+col password_version    clear;
+col default_ts          clear;
+col temp_ts             clear;
+col profile             clear;
+col rsrc_group          clear;

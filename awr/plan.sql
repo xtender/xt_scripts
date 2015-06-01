04fcf42b14ec91f75b44b94c4e2c5e1b50aebe6b
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-01 21:31:19 +0300

    +different dbids

--- awr/plan.sql
+++ awr/plan.sql
@@ -2,13 +2,20 @@
 prompt &_C_REVERSE ***                Show plan from awr              *** &_C_RESET
 prompt 
 prompt Syntax: @awr/plan sql_id [format [plan_hash_value [dbid]] ]
+col PLAN_TABLE_OUTPUT for a180;
+break on dbid skip 1;
 
+with dbids as (select distinct dbid from dba_hist_sqlstat st where st.sql_id='&1' and ('&4' is null or dbid=to_number('&4')))
 select * 
-from table(
-   dbms_xplan.display_awr(/*sql_id          => */ '&1'
-                         ,/*plan_hash_value => */ '&3'
-                         ,/*db_id           => */ '&4' --'&DB_ID'
-                         ,/*format          => */  nvl('&2','advanced')
-                         )
-  );
+from 
+   dbids
+  ,table(
+       dbms_xplan.display_awr(/*sql_id          => */ '&1'
+                             ,/*plan_hash_value => */ '&3'
+                             ,/*db_id           => */ dbids.dbid
+                             ,/*format          => */  nvl('&2','advanced')
+                             )
+      );
+clear break;
+clear col;
 @inc/input_vars_undef;

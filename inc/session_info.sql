2e7b8057194a0ed7bbdfc0c4cf233f8caa5612e7
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-15 19:16:44 +0300

    changed to sqlplus _USER

--- inc/session_info.sql
+++ inc/session_info.sql
@@ -3,7 +3,7 @@ define db_name     =""
 define db_inst_id  =""
 define db_host_name=""
 define db_version  =""
-define my_user     =""
+define my_user     ="&_USER"
 define my_sid      =""
 define my_host     =""
 define my_ip       =""
@@ -20,7 +20,6 @@ col db_name         new_val     db_name         noprint;
 col db_inst_id      new_val     db_inst_id      noprint;
 col db_host_name    new_val     db_host_name    noprint;
 col db_version      new_val     db_version      noprint;
-col my_user         new_val     my_user         noprint;
 col my_sid          new_val     my_sid          noprint;
 col my_host         new_val     my_host         noprint;
 col my_ip           new_val     my_ip           noprint;
@@ -44,7 +43,6 @@ select--+ rule ordered
   ,trim(i.instance_number)                      as db_inst_id
   ,i.host_name                                  as db_host_name
   ,i.version                                    as db_version
-  ,trim(user                                  ) as my_user
   ,trim(userenv('SID')                        ) as my_sid
   ,trim(sys_context('USERENV','HOST')         ) as my_host
   ,trim(sys_context('USERENV','IP_ADDRESS')   ) as my_ip
@@ -68,7 +66,6 @@ col db_name         clear;
 col db_inst_id      clear;
 col db_host_name    clear;
 col db_version      clear;
-col my_user         clear;
 col my_sid          clear;
 col my_host         clear;
 col my_ip           clear;

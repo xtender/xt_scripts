3d0ce1bc82a182cc606dfee8801f99cb4392c50c
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-15 17:16:33 +0300

    formatting

--- invalids.sql
+++ invalids.sql
@@ -1,4 +1,5 @@
 col object_type format a20;
+col object_name format a30;
 accept _owner prompt "Enter owner mask[%]: ";
 accept _otype prompt "Enter type  mask[%]: ";
 accept _oname prompt "Enter name  mask[%]: ";
@@ -13,5 +14,6 @@ where o.owner     like nvl(upper('&_owner'),'%')
   and object_type like nvl(upper('&_otype'),'%')
   and object_name like nvl(upper('&_oname'),'%')
   and o.status='INVALID';
+col object_name clear;
 col object_type clear;
 undef _owner _otype _oname;

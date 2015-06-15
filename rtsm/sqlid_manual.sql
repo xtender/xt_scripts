36114068c7782ec235c23c8a2c8cb977a7f3133a
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-15 17:17:27 +0300

    +px

--- rtsm/sqlid_manual.sql
+++ rtsm/sqlid_manual.sql
@@ -1,11 +1,17 @@
 @inc/input_vars_init;
 
+prompt *** Simplified RTSM report
+prompt * Usage: @rtsm/sqlid_manual SQL_ID [px]
+prompt * Add "px" to show stats by slaves
+prompt ;
+
 def sql_id=&1
 
 col if_parallel new_value _px noprint;
 select decode(count(*),0,'--','  ') if_parallel
 from dual 
 where exists(select null from v$sql_monitor r where r.sql_id='&sql_id' and r.PX_SERVERS_REQUESTED>0)
+  and lower('&2') like 'px'
 /
 col if_parallel clear;
 
@@ -19,12 +25,18 @@ col io                     for a12;
 col read                   for a12;
 col write                  for a12;
 
+col wa_mem                  for 999g999d0;
+col wa_max                  for 999g999d0;
+col wa_temp                 for 999g999d0;
+col wa_maxtemp              for 999g999d0;
+
 col per_slave_rows         for a50
 col per_slave_io_bytes     for a50
 col per_slave_read_reqs    for a50
 col per_slave_read_bytes   for a50
 col per_slave_write_reqs   for a50
 col per_slave_write_bytes  for a50
+
 
 break on sql_id on ch# on plan_hv on exec_id on started on status skip 1
 
@@ -41,19 +53,19 @@ sql_mon_manual
     ,plan_line_id
     ,plan_operation
     ,plan_options
-    ,plan_object_owner||'.'||plan_object_name as obj_name
-    ,count(sid)                    as proc_cnt
-    ,sum(starts                  ) as starts
-    ,sum(output_rows             ) as output_rows
-    ,sum(IO_INTERCONNECT_BYTES   ) as IO_INTERCONNECT_BYTES
-    ,sum(PHYSICAL_READ_REQUESTS  ) as PHYSICAL_READ_REQUESTS 
-    ,sum(PHYSICAL_READ_BYTES     ) as PHYSICAL_READ_BYTES    
-    ,sum(PHYSICAL_WRITE_REQUESTS ) as PHYSICAL_WRITE_REQUESTS
-    ,sum(PHYSICAL_WRITE_BYTES    ) as PHYSICAL_WRITE_BYTES   
-    ,sum(WORKAREA_MEM            ) as WORKAREA_MEM           
-    ,sum(WORKAREA_MAX_MEM        ) as WORKAREA_MAX_MEM       
-    ,sum(WORKAREA_TEMPSEG        ) as WORKAREA_TEMPSEG       
-    ,sum(WORKAREA_MAX_TEMPSEG    ) as WORKAREA_MAX_TEMPSEG   
+    ,plan_object_owner||'.'||plan_object_name   as obj_name
+    ,count(sid)                                 as proc_cnt
+    ,sum(starts                  )              as starts
+    ,sum(output_rows             )              as output_rows
+    ,sum(IO_INTERCONNECT_BYTES   )              as IO_INTERCONNECT_BYTES
+    ,sum(PHYSICAL_READ_REQUESTS  )              as PHYSICAL_READ_REQUESTS 
+    ,sum(PHYSICAL_READ_BYTES     )              as PHYSICAL_READ_BYTES    
+    ,sum(PHYSICAL_WRITE_REQUESTS )              as PHYSICAL_WRITE_REQUESTS
+    ,sum(PHYSICAL_WRITE_BYTES    )              as PHYSICAL_WRITE_BYTES   
+    ,sum(WORKAREA_MEM            )              as WORKAREA_MEM           
+    ,sum(WORKAREA_MAX_MEM        )              as WORKAREA_MAX_MEM       
+    ,sum(WORKAREA_TEMPSEG        )              as WORKAREA_TEMPSEG       
+    ,sum(WORKAREA_MAX_TEMPSEG    )              as WORKAREA_MAX_TEMPSEG   
 &_px    ,listagg(output_rows              ,'/') within group(order by sid) as per_slave_rows
 &_px    ,listagg(IO_INTERCONNECT_BYTES    ,'/') within group(order by sid) as per_slave_io_bytes
 &_px    ,listagg(PHYSICAL_READ_REQUESTS   ,'/') within group(order by sid) as per_slave_read_reqs
@@ -61,7 +73,8 @@ sql_mon_manual
 &_px    ,listagg(PHYSICAL_WRITE_REQUESTS  ,'/') within group(order by sid) as per_slave_write_reqs
 &_px    ,listagg(PHYSICAL_WRITE_BYTES     ,'/') within group(order by sid) as per_slave_write_bytes
    from v$sql_plan_monitor r
-   --where r.sql_id='1ypdagbrs5yms'
+   where r.sql_id='&sql_id'
+     and r.starts>0
    group by 
      sql_id,sql_exec_start,sql_exec_id, SQL_PLAN_HASH_VALUE, SQL_CHILD_ADDRESS
     ,status
@@ -81,7 +94,7 @@ select
    ,"***"
    ,p.ID
    ,LPAD(' ', depth) || p.operation ||' '|| p.options || NVL2(p.object_name, ' ['||p.object_name ||']', null) as operation
-&_px   ,mon.proc_cnt
+   ,mon.proc_cnt
    ,mon.starts
    ,mon.output_rows
    ,mon.PHYSICAL_READ_REQUESTS  as reqs_read
@@ -107,10 +120,10 @@ select
        when mon.PHYSICAL_WRITE_BYTES= (               1024) then to_char(mon.PHYSICAL_WRITE_BYTES / (               1024) ,'9g999d0')||'KB'
                                                             else to_char(mon.PHYSICAL_WRITE_BYTES                         ,'999g999')||' B'
     end as write
-   ,mon.WORKAREA_MEM           as wa_mem
-   ,mon.WORKAREA_MAX_MEM       as wa_max
-   ,mon.WORKAREA_TEMPSEG       as wa_temp
-   ,mon.WORKAREA_MAX_TEMPSEG   as wa_maxtemp
+   ,round(mon.WORKAREA_MEM        /1024/1024,1)   as wa_mem
+   ,round(mon.WORKAREA_MAX_MEM    /1024/1024,1)   as wa_max
+   ,round(mon.WORKAREA_TEMPSEG    /1024/1024,1)   as wa_temp
+   ,round(mon.WORKAREA_MAX_TEMPSEG/1024/1024,1)   as wa_maxtemp
 &_px   ,per_slave_rows
 &_px   ,per_slave_io_bytes
 &_px   ,per_slave_read_reqs

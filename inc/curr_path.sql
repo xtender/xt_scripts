ed39f40b3ff47441638606ee31ead7467b34c345
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-26 20:26:34 +0300

    replaced to grep

--- inc/curr_path.sql
+++ inc/curr_path.sql
@@ -6,17 +6,17 @@ spool _cur_path.remove
 @@notfound
 spool off;
 
-
-ho "cat _cur_path.remove 2>nul | sed 's#SP2-0310: unable to open file .##g' | sed 's#notfound.sql.##g' >_cur_path.remove"
+ho "grep -Po 'SP2-0310: unable to open file .\K.*(?=notfound.sql)' _cur_path.remove > cur_path.remove"
 
 var cur_path varchar2(100);
 begin :cur_path :=rtrim(ltrim( 
                         q'[
-                            @_cur_path.remove
+                            @cur_path.remove
                         ]',' '||chr(10)),' '||chr(10));
 end;
 /
 ho "rm _cur_path.remove"
+ho "rm cur_path.remove"
 col cur_path new_val cur_path noprint;
 select :cur_path cur_path from dual;
 set termout on;

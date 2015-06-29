0354c49b03d8217f4dbd9ac3cb0c40b28456d94c
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-29 03:47:42 +0300

    cross platform version without dependencies

--- inc/curr_path.sql
+++ inc/curr_path.sql
@@ -1,23 +1,30 @@
 rem Simple example how to get path (@@) of the current script.
 rem This script will set "cur_path" variable, so we can use &cur_path later.
-
+ 
 set termout off
 spool _cur_path.remove
 @@notfound
 spool off;
-
-ho "sed -i'' 's#SP2-0310: unable to open file .\(.*\)notfound.*#\1#' _cur_path.remove"
-
+ 
 var cur_path varchar2(100);
-begin :cur_path :=rtrim(ltrim( 
+declare 
+  v varchar2(100);
+  m varchar2(100):='SP2-0310: unable to open file "';
+begin v :=rtrim(ltrim( 
                         q'[
                             @_cur_path.remove
                         ]',' '||chr(10)),' '||chr(10));
+  v:=substr(v,instr(v,m)+length(m));
+  v:=substr(v,1,instr(v,'notfound.')-1);
+  :cur_path:=v;
 end;
 /
-ho "rm _cur_path.remove"
+set scan off;
+ho "rm _cur_path.remove 2>&1  | echo ."
+ho "del _cur_path.remove 2>&1 | echo ."
 col cur_path new_val cur_path noprint;
 select :cur_path cur_path from dual;
+set scan on;
 set termout on;
-
+ 
 prompt Current path: &cur_path

97a31a702e1d1f38e3bdc6466e6036232052a360
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-29 20:17:21 +0300

    one "host"

--- inc/curr_path.sql
+++ inc/curr_path.sql
@@ -1,11 +1,11 @@
 rem Simple example how to get path (@@) of the current script.
 rem This script will set "cur_path" variable, so we can use &cur_path later.
- 
+
 set termout off
 spool _cur_path.remove
 @@notfound
 spool off;
- 
+
 var cur_path varchar2(100);
 declare 
   v varchar2(100);
@@ -20,11 +20,10 @@ begin v :=rtrim(ltrim(
 end;
 /
 set scan off;
-ho (rm _cur_path.remove 2>&1  | echo .)
-ho (del _cur_path.remove 2>&1 | echo .)
+ho (del _cur_path.remove || rm _cur_path.remove) 2>&1 | echo .
 col cur_path new_val cur_path noprint;
 select :cur_path cur_path from dual;
 set scan on;
 set termout on;
- 
+
 prompt Current path: &cur_path

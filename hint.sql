21e058047eec4fcc5be672e89a4bb8016e5b6c1b
Sayan Malakshinov <xt.and.r@gmail.com>
2015-06-30 17:45:28 +0300

    fix

--- hint.sql
+++ hint.sql
@@ -3,7 +3,13 @@ col sql_feature format a35;
 col class       format a30;
 col version     format a20;
 col inverse     format a30;
-select name,sql_feature,class,version,inverse from v$sql_hint where name like upper('%&1%') or sql_feature like upper('%&1%') or class like upper('%&1%');
+select name,sql_feature,class,version,inverse 
+from v$sql_hint 
+where 
+      name          like upper('%&1%') 
+   or sql_feature   like upper('%&1%') 
+   or class         like upper('%&1%')
+   or version       like '&1%';
 col name        clear;
 col sql_feature clear;
 col class       clear;
--- obj.sql
+++ obj.sql
@@ -1,8 +1,11 @@
 @inc/input_vars_init;
-col owner       format a30;
-col object_name format a30;
-col object_type format a25;
-col last_ddl_time format a19;
+prompt *** Find object
+prompt * Usage: @obj object_mask/object_id [owner] [object_type]
+col owner           format a30;
+col object_name     format a30;
+col object_type     format a25;
+col last_ddl_time   format a19;
+col timestamp       format a19 trunc;
 
 select owner,object_name,object_type,object_id ,created
    ,to_char(last_ddl_time,'yyyy-mm-dd hh24:mi:ss') last_ddl_time
@@ -12,9 +15,11 @@ from dba_objects o
 where 
    (to_char(o.object_id) like '&1' or upper(object_name) like upper('&1'))
    and o.owner like nvl(upper('&2'),'%')
+   and object_type like upper('%&3%')
 ;
-col owner       clear;
-col object_name clear;
-col object_type clear;
-col last_ddl_time clear;
+col owner           clear;
+col object_name     clear;
+col object_type     clear;
+col last_ddl_time   clear;
+col timestamp       clear;
 @inc/input_vars_undef;

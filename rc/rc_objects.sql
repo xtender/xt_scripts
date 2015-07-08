82e31caa49a6440b326e04514041774322cb169c
Sayan Malakshinov <xt.and.r@gmail.com>
2015-07-08 17:26:29 +0300

    more columns

--- rc/rc_objects.sql
+++ rc/rc_objects.sql
@@ -1,2 +1,12 @@
-select id,type,status,name,namespace,creation_timestamp,scn from v$result_cache_objects o where o.name like '%'
+@inc/input_vars_init;
+col name for a128;
+select 
+    id,type,status
+    ,substr(name,1,max(length(name))over()) name
+    ,namespace,creation_timestamp,scn
+    ,pin_count,scan_count,invalidations
+from v$result_cache_objects o 
+where lower(o.name) like lower('%&1%')
 /
+col name clear;
+@inc/input_vars_undef;

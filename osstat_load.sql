6f6dd76f0e42db03d2cad4aab734b85941063955
Sayan Malakshinov <xt.and.r@gmail.com>
2015-08-06 15:45:17 +0300

    +RAC

--- osstat_load.sql
+++ osstat_load.sql
@@ -1,4 +1,10 @@
-select
-   (select value from V$OSSTAT where stat_name='LOAD'    ) load
-  ,(select value from V$OSSTAT where stat_name='NUM_CPUS') num_cpus
-from dual;
+col load for 9990d9;
+select 
+     inst_id
+    ,max(decode(stat_name,'LOAD'    ,value)) load
+    ,max(decode(stat_name,'NUM_CPUS',value)) num_cpus 
+from GV$OSSTAT 
+where stat_name in ('LOAD','NUM_CPUS')
+group by inst_id
+order by inst_id;
+col load clear;

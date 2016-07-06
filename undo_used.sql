03bb0f54efa83a86503a983cbf64c3e53c92a081
Sayan Malakshinov(xtender) <xt.and.r@gmail.com>
2016-07-06 12:28:19 +0300

    +formatting

--- undo_used.sql
+++ undo_used.sql
@@ -8,7 +8,18 @@ col process          format a13;
 col object           format a40;
 col subobject_name   format a30;
 col object_type      format a20;
-   select
+col l_mode           format a13;
+col xidusn  noprint;
+col xidslot noprint;
+col xidsqn  noprint;
+break on sid on serial# -
+      on username -
+      on osuser -
+      on recs -
+      on blocks -
+      on xidusn on xidslot on xidsqn -
+      on process;
+select
       s.sid
      ,s.serial#
      ,s.username
@@ -35,14 +46,14 @@ col object_type      format a20;
      ,o.data_object_id
      ,o.object_type
 from v$transaction t
-      , v$session s
-      , v$locked_object l
-      , dba_objects o
-    where t.addr      = s.taddr
-      and t.xidusn    = l.xidusn(+)
-      and t.xidslot   = l.xidslot(+)
-      and l.object_id = o.object_id(+)
-      and sid = '&1'+0
+   , v$session s
+   , v$locked_object l
+   , dba_objects o
+where t.addr      = s.taddr
+  and t.xidusn    = l.xidusn(+)
+  and t.xidslot   = l.xidslot(+)
+  and l.object_id = o.object_id(+)
+  and sid = '&1'+0
 /
 col username         clear;
 col osuser           clear;
@@ -50,4 +61,6 @@ col process          clear;
 col object           clear;
 col subobject_name   clear;
 col object_type      clear;
+col l_mode           clear;
+clear break;
 @inc/input_vars_undef;

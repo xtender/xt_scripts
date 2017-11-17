af013dbffdd8bcaaa8d727c516627b2d4879ce7e
Sayan Malakshinov <xt.and.r@gmail.com>
2017-11-17 03:37:31 +0300

    start from 1

--- events/enabled.sql
+++ events/enabled.sql
@@ -2,7 +2,7 @@ set serverout on;
 declare
   level# int;
 begin
-  for event# in 10000..10999 loop
+  for event# in 1..10999 loop
      sys.dbms_system.read_ev (event#, level#);
      if level# != 0  then
         dbms_output.put_line('Event #'||event#||' level:'||level# );

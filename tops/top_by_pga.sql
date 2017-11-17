c81ae603d81edc82c4a029f298047133d8cd392b
Sayan Malakshinov <xt.and.r@gmail.com>
2017-11-17 03:39:11 +0300

    sort

--- tops/top_by_pga.sql
+++ tops/top_by_pga.sql
@@ -1,18 +1,29 @@
-select * 
+break on sid on spid on pid on total_allocated skip 1;
+
+select sid,spid,pid,total_allocated,category,allocated,used,max_allocated
 from (
-      SELECT
-          s.sid,p.spid
-          ,sum(pm.allocated) over(partition by sid) total_allocated
-          ,pm.*
-      FROM 
-          v$session s
-        , v$process p
-        , v$process_memory pm
-      WHERE
-          s.paddr = p.addr
-      AND p.pid = pm.pid
-      ORDER BY
-          3 desc
-)
-where rownum<=10
+    select 
+         dense_rank()over(ORDER BY total_allocated desc) rnk
+        ,v.* 
+    from 
+        (
+              SELECT
+                  s.sid,p.spid,p.pid
+                  ,sum(pm.allocated) over(partition by sid) total_allocated
+                  ,pm.category
+                  ,pm.allocated
+                  ,pm.used
+                  ,pm.max_allocated
+              FROM 
+                  v$session s
+                , v$process p
+                , v$process_memory pm
+              WHERE
+                  s.paddr = p.addr
+              AND p.pid = pm.pid
+              ORDER BY total_allocated desc,allocated desc,used desc
+        ) v
+    )
+where rnk<=5
 /
+clear break;

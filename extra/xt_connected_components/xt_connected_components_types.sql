ccb4ff5f05d78bc1648119f2aba828da9b4a1cb6
Sayan Malakshinov <xt.and.r@gmail.com>
2019-06-25 17:39:01 +0300

    fix for sqlplus

--- extra/xt_connected_components/xt_connected_components_types.sql
+++ extra/xt_connected_components/xt_connected_components_types.sql
@@ -1,5 +1,8 @@
 create or replace type numbers is table of number;
+/
 create or replace type strings is table of varchar2(100);
-  
+/ 
 create or replace type numbers_array is table of numbers;
+/
 create or replace type strings_array is table of strings;
+/

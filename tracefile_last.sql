ed45880049c2ebb8d884c8d30aabf7d15db28b39
Sayan Malakshinov <xt.and.r@gmail.com>
2021-05-28 01:26:59 +0300

    argument

--- tracefile_last.sql
+++ tracefile_last.sql
@@ -1,3 +1,4 @@
+@inc/input_vars_init;
 col adr_home       for a40;
 col change_time    for a15;
 col modify_time    for a15;
@@ -9,4 +10,5 @@ select
     ,to_char(modify_time,'mon/dd hh24:mi:ss') modify_time
 from V$DIAG_TRACE_FILE 
 order by change_time desc
-fetch first 10 rows only;
+fetch first nvl2('&1',&1+0,10) rows only;
+@inc/input_vars_undef

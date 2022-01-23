98275dcf3b3397dca67c37faf99e8f96a067bf9d
rednetx <sayan@orasql.org>
2022-01-23 18:36:46 +0300

    new diagnostic event component Result_Cache

--- trace_rc_off.sql
+++ trace_rc_off.sql
@@ -1,4 +1,4 @@
-alter session set events '10843 trace name context off';
-alter session set events '10046 trace name context off';
-alter session set tracefile_identifier=CLEANUP;
 alter session set tracefile_identifier=new;
+--alter session set events '43905 trace name context off';
+--alter session set events '10843 trace name context off';
+alter session set events 'trace[Result_Cache] off';
